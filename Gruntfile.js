967dec762edc14f72a1a09540e6a95f50880986c
Carl Green <carlgreen@gmail.com>
2014-06-29 14:38:35 +1200

    add task timings

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,9 @@ module.exports = function(grunt) {
   // Load grunt tasks automatically
   require('load-grunt-tasks')(grunt);
 
+  // Time how long tasks take. Can help when optimizing build times
+  require('time-grunt')(grunt);
+
   // Define the configuration for all the tasks
   grunt.initConfig({
     // Watches files for changes and runs tasks based on the changed files
