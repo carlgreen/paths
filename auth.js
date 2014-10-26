f7c77250055a6ea56ba59b740951d885b297da33
Carl Green <carlgreen@gmail.com>
2014-10-27 12:49:09 +1300

    configure auth module with database connection

--- auth.js
+++ auth.js
@@ -1,14 +1,24 @@
 module.exports = function() {
-  return function(req, res, next) {
-    // don't need to be authenticated to authenticate
-    if (req.path === '/connect') {
+  var db;
+  return {
+    connectDb: function(connection) {
+      db = connection;
+    },
+    doAuth: function(req, res, next) {
+      // don't need to be authenticated to authenticate
+      if (req.path === '/connect') {
+        next();
+        return;
+      }
+      if (!('user_id' in req.session)) {
+        res.status(401);
+        return;
+      }
+      if (typeof db === 'undefined') {
+        res.status(500);
+        return;
+      }
       next();
-      return;
     }
-    if (!('user_id' in req.session)) {
-      res.status(401);
-      return;
-    }
-    next();
   };
 };
--- test/server/auth.js
+++ test/server/auth.js
@@ -1,17 +1,24 @@
 'use strict';
 
 var should = require('should'),
-  auth = require('../../auth')();
+  authModule = require('../../auth');
 
 describe('Auth', function() {
+  var auth;
+
+  beforeEach(function() {
+    auth = authModule();
+    auth.connectDb({});
+  });
+
   it('should not do anything for /connect', function(done) {
-    auth({
+    auth.doAuth({
       path: '/connect'
     }, {}, done);
   });
 
   it('should be ok if user_id is in the session', function(done) {
-    auth({
+    auth.doAuth({
       path: '/other',
       session: {
         user_id: '123'
@@ -20,12 +27,29 @@ describe('Auth', function() {
   });
 
   it('should send 401 if no user_id in the session', function(done) {
-    auth({
+    auth.doAuth({
       path: '/other',
       session: {}
     }, {
       status: function(status) {
         status.should.equal(401);
+        done();
+      }
+    }, null);
+  });
+
+  it('should send 500 if database is not configured', function(done) {
+    // make it like it was never configured
+    auth.connectDb(undefined);
+
+    auth.doAuth({
+      path: '/other',
+      session: {
+        user_id: '123'
+      }
+    }, {
+      status: function(status) {
+        status.should.equal(500);
         done();
       }
     }, null);
