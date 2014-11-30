ca84f351b2309c9d13d8b3a236c51be00dd6f403
Carl Green <carlgreen@gmail.com>
2014-12-01 08:38:38 +0900

    start page to print auth result

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,10 @@
 
   var app = angular.module('paths', ['ngRoute', 'pathsServices', 'pathsControllers']);
   app.config(function($routeProvider) {
-    $routeProvider.when('/admin/files', {
+    $routeProvider.when('/start', {
+      controller: 'StartController',
+      templateUrl: 'views/start.html'
+    }).when('/admin/files', {
       controller: 'AdminController',
       templateUrl: 'views/admin/files.html'
     }).otherwise({redirectTo:'/'});
